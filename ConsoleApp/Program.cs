be47ff6e50e23facc5621fe101a6e174e4078f22
susu <113452531+susanna3003@users.noreply.github.com>
2023-02-22 11:29:51 -0500

    Update Program.cs
    Added name block

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/**
+* 2/22/23
+* CSC 153
+* Susanna Quayle
+* Sprint 1
+*/
 namespace ConsoleApp
 {
     class Program
