baf41323a6e799223967bd88102c3102d2943a22
susanna3003 <quayles5806@student.faytechcc.edu>
2023-03-27 10:08:56 -0400

    library created, named, and referenced

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using Utopic_Library;
 
 /**
 * 2/22/23
--- Utopic_Library/Utopic_Library.cs
+++ Utopic_Library/Utopic_Library.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Utopic_Library
 {
-    public class Class1
+    public class Utopic_Library
     {
     }
 }
